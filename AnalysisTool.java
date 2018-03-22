bc3e7a1f32c55cdc7e023367126f4a6f6bd2c25d
Cazif <syslack_moe@hotmail.com>
2018-03-22 10:14:45 -0600

    Add files via upload

--- AnalysisTool.java
+++ AnalysisTool.java
@@ -1,100 +1,166 @@
-package AnalysisToolProject;
-
-
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.EnumDeclaration;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.IVariableBinding;
+import org.eclipse.jdt.core.dom.QualifiedName;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.ASTVisitor;
-import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 
+/**
+ * This tool takes a path directory and Java type and parses all the
+ * files with .java extensions. The amount of declarations
+ * and references withing all the files is printed to the console
+ * @author Josh, Logan, Madhu
+ *
+ */
 public class AnalysisTool {
-	
-	static String pathname;
-	public static void main(String[] args) {
-		  for(int i = 0; i < args.length; i++) {
-			  
-			  pathname = args[i];
-		  }
-		  System.out.println("Usage: java Driver <pathname>" + pathname);
-		  	  
-		
-		AnalysisTool testy = new AnalysisTool();
-		testy.test1();
 
+	private String pathname;
+	private String type;
+	private int dCount;
+	private int rCount;
+	
+	public static void main(String[] args) {
+		if(args.length != 2) {
+			throw new IllegalArgumentException();
+		}
+		
+		AnalysisTool toolA = new AnalysisTool(args[0],args[1]);
+		toolA.getFile(toolA.getPathName());
+		System.out.println(toolA.printResult());
 	}
-	public void test1(){
-		int count = 0;
+	
+	public String getPathName() {
+		return pathname;
+	}
+	
+	public String getType(){
+		return type;
+	}
+	public int getDCount() {
+		return dCount;
+	}
+	public int getRCount() {
+		return rCount;
+	}
+	
+	/**
+	 * Constructor for the Class, used to set variables
+	 * @param path directory to be searched
+	 * @param type to search for during parsing
+	 */
+	public AnalysisTool(String path, String type) {
+		dCount = 0;
+		rCount = 0;
+		pathname = path;
+		this.type = type;
+	}
+	/**
+	 * The results of the AnalysisTool
+	 * @return String containing the results
+	 */
+	public String printResult() {
+		return type+". Declarations found: "+dCount+"; references found: "+rCount+".";
+	}
+	
+	/**
+	 * Parses a file using ASTParser
+	 * @param fileName of the file to be parsed
+	 */
+	public void parseTool(String fileName) {
+	
 		ASTParser parser = ASTParser.newParser(AST.JLS9);
-		parser.setSource(fileToChar("input.java"));
+		parser.setSource(fileToChar(fileName));
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		parser.setResolveBindings(true);
-		
-		
+		parser.setBindingsRecovery(true);
+		parser.setEnvironment(null, null, null, true);
+		parser.setUnitName("lol");
 		final CompilationUnit cu = (CompilationUnit)parser.createAST(null);
-	
-		cu.accept(new ASTVisitor() {
-		 
-			Set names = new HashSet();
-	
-			public boolean visit(VariableDeclarationFragment node) {
-				
-				SimpleName name = node.getName();
-				this.names.add(name.getIdentifier());
-				System.out.println("Declaration of '" + name + "' at line"
-						+ cu.getLineNumber(name.getStartPosition()));
-				System.out.println(name.getFullyQualifiedName());
-				//System.out.println(name.isDeclaration());
-				//System.out.println(node.getNodeType());
-				/*if (node.getNodeType() == ASTNode.PRIMITIVE_TYPE)
-				{
-					System.out.println("Its primitive");
-				}*/
-				switch (node.getNodeType()) {
-				//case ASTNode.BOOLEAN_LITERAL:
-					
-				case ASTNode.CHARACTER_LITERAL:
-				//case ASTNode.NULL_LITERAL:
-				//case ASTNode.NUMBER_LITERAL:
-				{
-					System.out.println("it is char");
-					//name.getFullyQualifiedName();
-					return true;
-				}
-					
-
-				default:
-					return false;
-			}
-				
-				
-				//return false; // do not continue 
-			}
-	
-			public boolean visit(SimpleName node) {
-				if (this.names.contains(node.getIdentifier())) {
-					System.out.println("Usage of '" + node + "' at line "
-							+ cu.getLineNumber(node.getStartPosition()));
-				}
+		cu.accept(new ASTVisitor() {			
+			
+			/**
+			 * Gets the name of the node and compares to
+			 * the type to look for, if the same
+			 * increases dCount by 1
+			 * @param node of the type TypeDeclaration
+			 * @return true to visit the children nodes
+			 */
+			public boolean visit(TypeDeclaration node) {
+				String nodeName = node.getName().getFullyQualifiedName();
+				if(nodeName.equals(getType())) dCount++;
 				return true;
 			}
+			
+			/**
+			 * Used for Enum declarations
+			 * Gets the name of the node and compares to
+			 * the type to look for, if the same
+			 * increases dCount by 1
+			 * @param node of the type EnumDeclaration
+			 * @return true to visit the children nodes
+			 */
+			public boolean visit(EnumDeclaration node) {
+				ITypeBinding typeBind = node.resolveBinding();
+				
+				String typeNode = typeBind.getQualifiedName();
+				if(typeNode.equals(getType())) dCount++;
+				
+				return true;
+			}
+			
+			/**
+			 * Used for Annotation declarations
+			 * Gets the name of the node and compares to
+			 * the type to look for, if the same
+			 * increases dCount by 1
+			 * @param node of the type AnnotationTypeDeclaration
+			 * @return true to visit the children nodes
+			 */
+			public boolean visit(AnnotationTypeDeclaration node) {
+				ITypeBinding typeBind = node.resolveBinding();
+				String typeNode = typeBind.getQualifiedName();				
+				if(typeNode.equals(getType())) dCount++;
+				return true;
+			}
+			
+			/**
+			 * Used for referential count
+			 * @param node of type QualififedName
+			 * @return false to not visit children
+			 */
+			public boolean visit(QualifiedName node) {
+				if(node.getFullyQualifiedName().equals(getType())) rCount++;
+				return false;
+			}
+			
+			/**
+			 * @param node of type VariableDeclarationFragment
+			 * Resolves the bindings so we can acquire the node's full name
+			 * @return false, don't visit children
+			 */
+			public boolean visit(VariableDeclarationFragment node) {
+				IVariableBinding rNode = node.resolveBinding();
+				String typeNode = rNode.getType().getQualifiedName();
+				if(typeNode.equals(getType())) rCount++;
+				return false;
+			}
+			
 		});
 	}
-	
+	/**
+	 * Converts the file name to a char array
+	 * @param file to be converted
+	 * @return	char[] of the file name
+	 */
 	public char[] fileToChar(String file) {
 	    // char array to store the file contents in
 	    char[] contents = null;
@@ -114,8 +180,26 @@ public class AnalysisTool {
 	    catch(IOException e) {
 	            System.out.println(e.getMessage());
 	    }
-	 
 	    return contents;
+	}
+	
+	/**
+	 * Retrieves all the .java files within the given directory
+	 * We retrieved a portion of this code from the following link
+	 * https://stackoverflow.com/questions/1844688/how-to-read-all-files-in-a-folder-from-java
+	 * @param path directory to look for files
+	 */
+	public void getFile(String path) {
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+		    if (file.isFile()) {
+		    		if(file.getName().matches(".*\\.java")) {
+		    			parseTool(path+"\\"+file.getName());
+		    		}
+		    }
+		}
 	}
 
 }
